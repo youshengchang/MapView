7e71b7b14641ebe4b0c3df9f0abf711dfbec028f
youshengchang <changyousheng@yahoo.com>
2015-03-01 13:40:39 -0800

    Initial commit

---
+++ app/src/main/java/com/example/gmapsapp/MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.gmapsapp;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.widget.Toast;
+
+public class MainActivity extends FragmentActivity {
+
+	private static final int GPS_ERRORDIALOG_REQUEST = 9001;
+	GoogleMap mMap;
+	MapView mMapView;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		if (servicesOK()) {
+			Toast.makeText(this, "Ready to map!", Toast.LENGTH_SHORT).show();
+			setContentView(R.layout.activity_mapview);
+			mMapView = (MapView)findViewById(R.id.map);
+			mMapView.onCreate(savedInstanceState);
+			
+		}
+		else {
+			setContentView(R.layout.activity_main);
+		}
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public boolean servicesOK() {
+		int isAvailable = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+		
+		if (isAvailable == ConnectionResult.SUCCESS) {
+			return true;
+		}
+		else if (GooglePlayServicesUtil.isUserRecoverableError(isAvailable)) {
+			Dialog dialog = GooglePlayServicesUtil.getErrorDialog(isAvailable, this, GPS_ERRORDIALOG_REQUEST);
+			dialog.show();
+		}
+		else {
+			Toast.makeText(this, "Can't connect to Google Play services", Toast.LENGTH_SHORT).show();
+		}
+		return false;
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		mMapView.onDestroy();
+	}
+
+	@Override
+	public void onLowMemory() {
+		// TODO Auto-generated method stub
+		super.onLowMemory();
+		mMapView.onLowMemory();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		mMapView.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		mMapView.onResume();
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		// TODO Auto-generated method stub
+		super.onSaveInstanceState(outState);
+		mMapView.onSaveInstanceState(outState);
+	}
+
+	
+	
+}
